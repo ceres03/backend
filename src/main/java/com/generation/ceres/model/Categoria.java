2a4912656f65a154cf0b3311a028b7799f36b9ac
Julio <juliohcf@hotmail.com>
2024-07-05 11:42:19 -0300

    Feat: Anotações Spring

--- src/main/java/com/generation/ceres/model/Categoria.java
+++ src/main/java/com/generation/ceres/model/Categoria.java
@@ -1,10 +1,24 @@
 package com.generation.ceres.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="tb_categorias")
 public class Categoria {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message="O campo Nome não pode estar em branco")
 	private String nome;
+	@NotBlank(message="O campo Produtor não pode estar em branco")
 	private String produtor;
+
 	
 	public Long getId() {
 		return id;
