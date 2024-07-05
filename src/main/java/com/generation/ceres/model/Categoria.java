e68eb0a77f851ada237b320b1906d0e941fd285d
Caroline <carolinebauru@hotmail.com>
2024-07-05 11:11:55 -0300

    feat: getters and setters

--- src/main/java/com/generation/ceres/model/Categoria.java
+++ src/main/java/com/generation/ceres/model/Categoria.java
@@ -5,4 +5,26 @@ public class Categoria {
 	private Long id;
 	private String nome;
 	private String produtor;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getProdutor() {
+		return produtor;
+	}
+	public void setProdutor(String produtor) {
+		this.produtor = produtor;
+	}
+	
+	
 }
+
