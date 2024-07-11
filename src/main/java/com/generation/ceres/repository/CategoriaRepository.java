657e98e61a62062d5313c466e0c18a99b48a4911
ouxnq <buenop.vinicius@gmail.com>
2024-07-11 11:15:01 -0300

    feat: Delete do CRUD

---
+++ src/main/java/com/generation/ceres/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.ceres.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.ceres.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
