e4b53e1ac37cb5484b29c0a5a9ea9b5ad8e54872
Caroline <carolinebauru@hotmail.com>
2024-07-12 14:14:54 -0300

    met get all, get by id, delete

--- src/main/java/com/generation/ceres/controller/ProdutoController.java
+++ src/main/java/com/generation/ceres/controller/ProdutoController.java
@@ -1,12 +1,56 @@
 package com.generation.ceres.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.ceres.model.Produto;
+import com.generation.ceres.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/produtos")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
 
+	@Autowired
+    private ProdutoRepository produtoRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll() {
+	    List<Produto> produtos = produtoRepository.findAll();
+	    if (produtos.isEmpty()) {
+	        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	    }
+	    return ResponseEntity.ok().body(produtos);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById(@PathVariable Long id) {
+	    Optional<Produto> produto = produtoRepository.findById(id);
+	    if (produto.isPresent()) {
+	        return ResponseEntity.ok().body(produto.get());
+	    }
+	    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+	    Optional<Produto> produto = produtoRepository.findById(id);
+	    if (produto.isPresent()) {
+	        produtoRepository.deleteById(id);
+	        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	    }
+	    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+
+	
 }
