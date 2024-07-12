137b853167832375472591775f55833f1102ffda
rojinhas <ruthrojasdasilva12@gmail.com>
2024-07-12 12:54:59 -0300

    Put and Post

--- src/main/java/com/generation/ceres/controller/ProdutoController.java
+++ src/main/java/com/generation/ceres/controller/ProdutoController.java
@@ -1,12 +1,42 @@
 package com.generation.ceres.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.ceres.model.Produto;
+import com.generation.ceres.repository.ProdutoRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
+	@PostMapping
+	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(produtoRepository
+				.save(produto));
+	}
+
+	@PutMapping
+	public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto) {
+		return produtoRepository.findById(produto.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+				.body(produtoRepository.save(produto)))
+				.orElse(ResponseEntity.notFound().build());
+
+	}
 }
